21d89a49e1b78c5a8bab84968d4bcaf780200a3f
YashPawar10 <yashrpawar13@gmail.com>
2024-12-18 11:49:23 +0530

    second commit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express  = require('express')
 const app = express()
 
 app.get('/',(req, res)=>{{
-    res.send("Hello world")
+    res.send("Hello  from yash pawar")
 }})
 
 app.listen(8000,()=>{
